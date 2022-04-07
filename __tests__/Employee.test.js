36df1348168346057cbea83f3ed736b82170a48d
Vicente Garcia <vicen1@hotmail.com>
2022-04-07 18:01:46 -0500

    Add Employee object and test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+test("Creates a Employee object", () => {
+    const employee = new Employee("Vicente","1","vicen1@hotmail.com","Manager");
+    expect(employee.name).toBe("Vicente");
+    expect(employee.id).toBe("1");
+    expect(employee.email).toBe("vicen1@hotmail.com");
+    expect(employee.role).toBe("Manager");
+});
+test("Gets employee's name value", () => {
+    const employee = new Employee("Vicente");
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+test("Gets employee's id value", () => {
+    const employee = new Employee("Vicente","1");
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+test("Gets employee's email value", () => {
+    const employee = new Employee("Vicente","1","vicen1@hotmail.com");
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+test("Gets employee's role value", () =>{
+    const employee = new Employee("Vicente","1","vicen1@hotmail.com","1");
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role.toString()));
+});
