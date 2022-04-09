20589d8d8a3a226de038db15ce1027e4c166d9d9
Vicente Garcia <vicen1@hotmail.com>
2022-04-09 17:22:37 -0500

    Add Engineer test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,29 @@
+const Engineer = require('../lib/Engineer');
+test("Creates a Engineer object", () => {
+    const engineer = new Engineer("Vicente","1","vicen1@hotmail.com","vicengs");
+    expect(engineer.name).toBe("Vicente");
+    expect(engineer.id).toBe("1");
+    expect(engineer.email).toBe("vicen1@hotmail.com");
+    expect(engineer.github).toBe("vicengs");
+    expect(engineer.role).toBe("Engineer");
+});
+test("Gets engineer's name value", () => {
+    const engineer = new Engineer("Vicente");
+    expect(engineer.getName()).toEqual(expect.stringContaining(engineer.name.toString()));
+});
+test("Gets engineer's id value", () => {
+    const engineer = new Engineer("Vicente","1");
+    expect(engineer.getId()).toEqual(expect.stringContaining(engineer.id.toString()));
+});
+test("Gets manager's email value", () => {
+    const engineer = new Engineer("Vicente","1","vicen1@hotmail.com");
+    expect(engineer.getEmail()).toEqual(expect.stringContaining(engineer.email.toString()));
+});
+test("Gets manager's github value", () => {
+    const engineer = new Engineer("Vicente","1","vicen1@hotmail.com","vicengs");
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+test("Gets manager's role value", () =>{
+    const engineer = new Engineer();
+    expect(engineer.getRole()).toEqual(expect.stringContaining(engineer.role.toString()));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,11 @@
 const Manager = require('../lib/Manager');
 test("Creates a Manager object", () => {
-    const manager = new Manager("Vicente","1","vicen1@hotmail.com","Manager","005");
+    const manager = new Manager("Vicente","1","vicen1@hotmail.com","005");
     expect(manager.name).toBe("Vicente");
     expect(manager.id).toBe("1");
     expect(manager.email).toBe("vicen1@hotmail.com");
-    expect(manager.role).toBe("Manager");
     expect(manager.officeNumber).toBe("005");
+    expect(manager.role).toBe("Manager");
 });
 test("Gets manager's name value", () => {
     const manager = new Manager("Vicente");
@@ -19,11 +19,11 @@ test("Gets manager's email value", () => {
     const manager = new Manager("Vicente","1","vicen1@hotmail.com");
     expect(manager.getEmail()).toEqual(expect.stringContaining(manager.email.toString()));
 });
-test("Gets manager's role value", () =>{
-    const manager = new Manager("Vicente","1","vicen1@hotmail.com","Manager");
-    expect(manager.getRole()).toEqual(expect.stringContaining(manager.role.toString()));
-});
 test("Gets manager's office number value", () => {
-    const manager = new Manager("Vicente","1","vicen1@hotmail.com","Manager","1005");
+    const manager = new Manager("Vicente","1","vicen1@hotmail.com","1005");
     expect(manager.getOfficeNumber()).toEqual(expect.stringContaining(manager.officeNumber.toString()));
+});
+test("Gets manager's role value", () =>{
+    const manager = new Manager();
+    expect(manager.getRole()).toEqual(expect.stringContaining(manager.role.toString()));
 });
