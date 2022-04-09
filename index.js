55180d9cee23f374af4c6bc6a6cad62c9b11acfa
Vicente Garcia <vicen1@hotmail.com>
2022-04-09 17:45:01 -0500

    Add index initialization

---
+++ index.js
@@ -0,0 +1,11 @@
+/* --------------------------------- */
+/* Project  : Team Profile Generator */
+/* File     : index.js               */
+/* Author   : Vicente Garcia         */
+/* Date     : 04/07/2022             */
+/* Modified : 04/09/2022             */
+/* --------------------------------- */
+// Add Team library to use
+const Team = require('./lib/Team');
+// Create new object (call to initialize a Team profile page) Team
+new Team().initializeTeam();
